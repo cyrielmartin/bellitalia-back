d7149219251d1787f576f8f6a99e2b50b0967ad6
Cyriel Martin <cyriel.martin@gmail.com>
2019-07-09 21:36:27 +0200

    Install Laravel, config routes, config views

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('admin')
+<div class="content">
+    <div class="title m-b-md">
+        Bell'Italia Admin
+    </div>
+
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="https://github.com/cyrielmartin/bellitalia">GitHub</a>
+    </div>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('home')
+<div class="content">
+    <div class="title m-b-md">
+        Bell'Italia
+    </div>
+
+    <div class="links">
+        <a href="/admin">Admin</a>
+        <a href="https://github.com/cyrielmartin/bellitalia">GitHub</a>
+    </div>
+</div>
+@endsection
