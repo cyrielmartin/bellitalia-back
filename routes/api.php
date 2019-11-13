297934c80a3f5751ad96a2052da476238abc8057
Cyriel Martin <cyriel.martin@gmail.com>
2019-11-13 22:22:00 +0100

    commit prétexte

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('interest', 'InterestController@index');
-// Route::get('interest/{id}', 'InterestController@show');
-// Route::post('interest', 'InterestController@create');
-// Route::put('interest/{id}', 'InterestController@update');
-// Route::delete('interest/{id}', 'InterestController@destroy');
-
 Route::apiResource('interest', 'Api\InterestController')->middleware('cors');
 Route::apiResource('region', 'Api\RegionController')->middleware('cors');
 Route::apiResource('tag', 'Api\TagController')->middleware('cors');
