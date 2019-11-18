6f33061228e89739bd586649922443094beacd4d
Cyriel Martin <cyriel.martin@gmail.com>
2019-11-18 22:42:13 +0100

    api bellitalia store Ok

--- app/Http/Controllers/Api/BellitaliaController.php
+++ app/Http/Controllers/Api/BellitaliaController.php
@@ -18,4 +18,29 @@ class BellitaliaController extends Controller
     // code 200 : succès de la requête
     return response()->json(Bellitalia::get(), 200);
   }
+
+  /**
+  * Enregistrement d'une nouvelle ressource (POST)
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    // Récupération requête
+    $data = $request->all();
+
+    // Enregistrement du Bellitalia nouvellement créé
+    if (isset($data['number'])) {
+      if(isset($data['date'])) {
+        // Formattage de la date pour BDD
+        $formattedDate  = date('Y-m-d', strtotime($data['date']));
+        //firstOrCreate pour éviter tout doublon accidentel
+        //(même si normalement doublons rendus impossibles par Vue Multiselect)
+        $bellitalia = BellItalia::firstOrCreate(array("number" => $data['number'], "publication" => $formattedDate));
+      }
+    }
+    return response()->json($bellitalia, 201);
+
+  }
 }
