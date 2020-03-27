f776bf0110c95e6a6d81d4446702e0ed5dfd7e49
Cyriel Martin <cma@skylab-x.io>
2020-03-27 23:02:38 +0100

    upload image publication WIp

--- app/Http/Controllers/Api/BellitaliaController.php
+++ app/Http/Controllers/Api/BellitaliaController.php
@@ -29,6 +29,7 @@ class BellitaliaController extends Controller
   {
     // Récupération requête
     $data = $request->all();
+    dd($data);
     // Enregistrement du Bellitalia nouvellement créé
     if (isset($data['number'])) {
       if(isset($data['date'])) {
