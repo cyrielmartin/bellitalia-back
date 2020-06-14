11571917ea8824cab6c64773e9b3166c0fbb53b0
Cyriel Martin <cyriel.martin@gmail.com>
2020-06-14 14:37:42 +0200

    SupplementController WIP

---
+++ app/Http/Controllers/Api/SupplementController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Supplement;
+use Validator;
+
+class SupplementController extends Controller
+{
+  /**
+  * Affiche la liste des ressources (GET)
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    // code 200 : succès de la requête
+    return response()->json(Supplement::get(), 200);
+  }
+
+  /**
+  * Enregistrement d'une nouvelle ressource (POST)
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    // Règles de validation :
+    // La règle image64 est une règle custom définie dans App\Providers\AppServiceProvider
+    $rules = [
+      'name' => 'required',
+      'publication' => 'required',
+      'image' => 'required|max:30000000|image64:jpg,jpeg,png',
+    ];
+
+    // Messages d'erreur custom
+    $messages = [
+      'name.required' => "Vous devez donner un nom au supplément",
+      'publication.required' => "Vous devez associer le supplément à un numéro de Bell'Italia",
+      'image.required' => "Vous devez associer une couverture au supplément",
+      'image.max' => "L'image dépasse le poids autorisé (30Mo)",
+      'image.image64' => "L'image doit être au format jpg, jpeg ou png",
+    ];
+
+    // J'applique le Validator à toutes les requêtes envoyées.
+    $validator = Validator::make($request->all(), $rules, $messages);
+    // Si 1 des règles de validation n'est pas respectée
+    if($validator->fails()){
+      //code 400 : syntaxe requête erronée
+      return response()->json($validator->errors(), 400);
+    }
+    // Récupération requête
+    $data = $request->all();
+    dd($data);
+    // Si une image est envoyée
+    // if($request->get('image'))
+    // {
+    //   // On la renomme et on la stocke
+    //   $imageArray = $request->get('image');
+    //   // Même s'il n'y a qu'une image envoyée, elle est stockée dans un tableau, donc foreach nécessaire
+    //   foreach ($imageArray as $key => $oneImage) {
+    //     // On la renomme en évitant toute possibilité de doublons :
+    //     // Nom du point d'intérêt + index + date + heure
+    //     // On fait bien attention de "nettoyer" le nom du point d'intérêt pour éviter tout pb dans la base :
+    //     // Pas d'espace, en minuscule, pas d'accent ou de caractères spéciaux (s'il y en a, la lettre est supprimée)
+    //     $name = 'publication'.trim($data['number']).'-'.date("Ymd-His", strtotime('+2 hours')).'.' . explode('/', explode(':', substr($oneImage, 0, strpos($oneImage, ';')))[1])[1];
+    //     \Image::make($oneImage)->save('./assets/publications/'. $name);
+    //
+    //     // On stocke l'URL vers l'image associée au point d'intérêt
+    //     $imagePath = url('/assets/publications/'.$name);
+    //     $data['image'] = $imagePath;
+    //   }
+
+
+      // $name = time().'.' . explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
+      // \Image::make($request->get('image'))->save('./assets/publications/'. $name);
+      //
+      // // On stocke l'URL vers l'image
+      // $imagePath = url('/assets/publications/'.$name);
+      // $data['image'] = $imagePath;
+    // }
+
+    // Enregistrement du Bellitalia nouvellement créé
+    // if (isset($data['number'])) {
+    //   if(isset($data['date'])) {
+    //     // Formattage de la date pour BDD :
+    //     // J'ajoute 1 jour (bizarrement, toutes les dates renvoyées par le front sont à J-1)
+    //     $date = $data['date'];
+    //     $formattedDate  = date('Y-m-d', strtotime($date . ' +1 day'));
+    //     //firstOrCreate pour éviter tout doublon accidentel
+    //     //(même si normalement doublons rendus impossibles par Vue Multiselect)
+    //     $bellitalia = BellItalia::firstOrCreate(array("number" => $data['number'], "publication" => $formattedDate, "image" => $imagePath));
+    //   }
+    // }
+    return response()->json($supplement, 201);
+
+  }
+}
