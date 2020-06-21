157e7ecbe5143aa3f165af461832728a99c6c7ab
Cyriel Martin <cyriel.martin@gmail.com>
2020-06-21 14:19:32 +0200

    Retour en arrière sur Tag modèle car pb de chargement

--- app/Http/Resources/Tag.php
+++ app/Http/Resources/Tag.php
@@ -3,27 +3,17 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\Interest as InterestResource;
 
 class Tag extends JsonResource
 {
-  /**
-  * Transform the resource into an array.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @return array
-  */
-  public function toArray($request)
-  {
-    return [
-      'id' => $this->id,
-      'name' => $this->name,
-      'interests' => InterestResource::collection($this->interests),
-    ];
-  }
-
-  public function withResponse($request, $response)
-  {
-    $response->header('X-Value', 'True');
-  }
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
 }
