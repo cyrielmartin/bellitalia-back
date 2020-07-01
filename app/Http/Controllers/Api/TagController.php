3a9bb6c174617754dca5b4dff60f3912c7e9f5c8
Cyriel Martin <cyriel.martin@gmail.com>
2020-07-01 23:18:23 +0200

    WIP update tag

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -54,4 +54,53 @@ class TagController extends Controller
 
   }
 
+  /**
+  * Mise à jour d'une ressource (PUT)
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id)
+  {
+    $tag = Tag::FindOrFail($id);
+    if(is_null($tag)){
+      return response()->json(['message' => 'Tag Not found'], 404);
+    }
+
+    // Récupération requête
+    $data = $request->all();
+    dd($data);
+    // Enregistrement des tags (catégories) nouvellement créés
+    if (isset($data['tag_id'])) {
+      // Pour chacun des tags récupérés ici
+      foreach ($data['tag_id'] as $tag) {
+        // On formatte le tag comme la BDD l'attend : name : xxx
+        // On le stocke dans un tableau
+        $formattedTag = ["name" => $tag];
+        // On stocke chacun de ces tags en BDD
+        foreach ($formattedTag as $newTag) {
+          //firstOrCreate pour éviter tout doublon accidentel
+          //(même si normalement doublons rendus impossibles par Vue Multiselect)
+          Tag::firstOrCreate($newTag)->id;
+        }
+      }
+    }
+
+    return response()->json($tag, 201);
+
+  }
+  /**
+  * Affichage d'une ressource (GET)
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id)
+  {
+
+    return new TagResource(Tag::FindOrFail($id));
+
+  }
+
 }
